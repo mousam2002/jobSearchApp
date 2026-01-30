62c625e551f1038172319d4af923ab8173f9bb31
mousam2002 <mausamtam@gmail.com>
2026-01-30 15:56:49 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config({});
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+
+const corsOptions = {
+   origin: ["http://localhost:4040"],
+   credentials: true,
+}
+
+app.use(cors(corsOptions));
+
+const PORT = process.env.POST || 3001;
+app.listen(PORT, () => {
+   console.log(`Server is running on port ${PORT}`);
+});
